95df86ccd7d542548a4ac544375ff15138b6bdce
Solemensis <96355888+Solemensis@users.noreply.github.com>
2022-09-06 11:36:49 +0300

    y

--- src/main.js
+++ src/main.js
@@ -1,14 +1,11 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import router from './router'
+import router from "./router";
 
 import "./main.css";
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(router)
+app.use(router);
 
-app.mount('#app')
-
-
-
+app.mount("#app");
--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./drink-e-commerce/",
+  base: "./",
   plugins: [vue()],
   resolve: {
     alias: {
